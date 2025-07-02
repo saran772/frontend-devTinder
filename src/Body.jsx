080040f5783d47346c051442d03faf6e0ec7e295
saran772 <saranpreetsinghsabharwal@gmail.com>
2025-07-02 11:34:49 +0530

    routing

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import Footer from "./footer"
+import Navbar from "./Navbar"
+import { Outlet } from "react-router-dom"
+
+const Body = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Body
